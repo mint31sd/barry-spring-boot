fa881760fdaae705a7f0ed998d0c65a39ece9d96
anonymously0031 <anonymously0031@protonmail.com>
2019-01-03 16:04:15 +0800

    update

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -15,9 +15,9 @@ import java.util.Date;
 public class HelloController {
     @RequestMapping("/hello")
     public String index(){
-        System.out.println("进入controller1234567");
-        System.out.println("进入controller1234567");
-        return "Hello world,barry hawkins1234567!";
+        System.out.println("进入controller12345678");
+        System.out.println("进入controller12345678");
+        return "Hello world,barry hawkins12345678!";
     }
 
     /**
